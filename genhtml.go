3fb94d03555fe547212fc9dd84bc0bed094a1b63
wangchao <watcher.hangzhou@foxmail.com>
2017-12-24 15:31:34 +0800

    fix

--- genhtml.go
+++ genhtml.go
@@ -92,6 +92,7 @@ func main()  {
 }
 func (r TraceHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	println("get",req.URL.Path," from ",req.RemoteAddr)
+
 	r.h.ServeHTTP(w, req)
 }
 type TraceHandler struct {
