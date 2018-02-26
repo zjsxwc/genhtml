0dd24453329ca5539d7d526d1fa31a6cc6d1ad9a
Wang Chao <watcher.hangzhou@foxmail.com>
2018-02-26 14:54:09 +0800

    code style fix

--- genhtml.go
+++ genhtml.go
@@ -13,7 +13,7 @@ import (
 	"path/filepath"
 )
 
-func ListDir(dirPth string) (files []string, names []string,  err error) {
+func listDir(dirPth string) (files []string, names []string, err error) {
 	suffixList := []string{".mp4", ".avi", ".wmv",".mkv",".mov",".flv"}
 
 	files = make([]string, 0, 10)
@@ -24,7 +24,7 @@ func ListDir(dirPth string) (files []string, names []string,  err error) {
 		return nil, nil, err
 	}
 
-	PthSep := string(os.PathSeparator)
+	pthSep := string(os.PathSeparator)
 	for _, fi := range dir {
 		if fi.IsDir() { // 忽略目录
 			continue
@@ -32,14 +32,14 @@ func ListDir(dirPth string) (files []string, names []string,  err error) {
 		for _, suffix := range suffixList{
 			suffix = strings.ToUpper(suffix)
 			if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
-				files = append(files, dirPth+PthSep+fi.Name())
+				files = append(files, dirPth + pthSep + fi.Name())
 				names = append(names, fi.Name())
 			}
 		}
 
 	}
 
-	return files,  names,nil
+	return files,  names, nil
 }
 
 func checkErr(err error) {
@@ -58,22 +58,31 @@ func getCurrentPath() string {
 	return ret
 }
 
+func (r TraceHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	println("get",req.URL.Path," from ",req.RemoteAddr)
+
+	r.h.ServeHTTP(w, req)
+}
+
+type TraceHandler struct {
+	h http.Handler
+}
 
 func main()  {
 	fmt.Println("genhtml")
 	path := getCurrentPath()
-	_, names, err := ListDir(path)
+	_, names, err := listDir(path)
 
-	jsondata, err := json.Marshal(names)
+	jsonData, err := json.Marshal(names)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	fmt.Println(string(jsondata))
+	fmt.Println(string(jsonData))
 	fmt.Println(path)
 
-	templateHtml,err := ioutil.ReadFile("template.html")
-	html := strings.Replace(string(templateHtml), "[[NAMES]]", string(jsondata), -1)
+	templateHtml, err := ioutil.ReadFile("template.html")
+	html := strings.Replace(string(templateHtml), "[[NAMES]]", string(jsonData), -1)
 	f, err := os.Create("index.html")
 	f.WriteString(html)
 	f.Close()
@@ -89,12 +98,4 @@ func main()  {
 		log.Fatal("ListenAndServe: ", http.ListenAndServe(":"+ *port, nil))
 	}
 
-}
-func (r TraceHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	println("get",req.URL.Path," from ",req.RemoteAddr)
-
-	r.h.ServeHTTP(w, req)
-}
-type TraceHandler struct {
-	h http.Handler
 }
